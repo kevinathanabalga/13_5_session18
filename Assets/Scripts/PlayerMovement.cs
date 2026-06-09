10bc28e763d48be1720b9db6ed0a01c584fbff60
nathaPrawira <kevinatha.nabalga.2002@gmail.com>
2026-06-09 21:46:44 +0700

    backup
    backup

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,51 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 1f;
-    public float jumpForce = 3f;
+    public float speed = 5f;
+    public float jumpForce = 7f;
+
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
 
     private Rigidbody2D rb;
-
     private SpriteRenderer spriteRenderer;
+
+    private float horizontalInput;
+    private bool jumpRequested;
+
     void Start()
     {
-        // Ambil komponen rigidbody dari objek player
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-
-    //sprite flip ini berguna untuk mengubah hadapan player
-    private void SpriteFlip(float horizontalInput)
+    void Update()
     {
-        if (horizontalInput < 0)
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            spriteRenderer.flipX = false;
+            jumpRequested = true;
         }
-        else
-        {
-            spriteRenderer.flipX = true;
-        }
+
+        SpriteFlip(horizontalInput);
     }
-
-
 
     void FixedUpdate()
     {
-        // Menggerakan player ke kanan atau kiri menggunakan transform.translate
-        float horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(new Vector3(horizontalInput * speed * Time.deltaTime, 0f, 0f));
-        SpriteFlip(horizontalInput);
+        rb.linearVelocity = new Vector2(
+            horizontalInput * speed,
+            rb.linearVelocity.y
+        );
 
+        bool isGrounded = Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundCheckRadius,
+            groundLayer
+        );
 
-        // Mengaktifkan lompatan player jika player menyentuh tanah
-        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.linearVelocity.y) < 0.001f)
+        if (jumpRequested && isGrounded)
         {
-            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            rb.linearVelocity = new Vector2(
+                rb.linearVelocity.x,
+                0f
+            );
 
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
+
+        jumpRequested = false;
+    }
+
+    private void SpriteFlip(float horizontalInput)
+    {
+        if (horizontalInput < 0)
+            spriteRenderer.flipX = false;
+        else if (horizontalInput > 0)
+            spriteRenderer.flipX = true;
     }
 }
